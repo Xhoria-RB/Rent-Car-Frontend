94c439af3d997950f2a20d313123ad0c42b7bd39
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-09-29 21:21:16 -0400

    refactoring done

--- components/BaseTable.js
+++ components/BaseTable.js
@@ -1,113 +1,57 @@
-/* eslint-disable no-underscore-dangle */
-import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import React from 'react';
 import useAxios from 'axios-hooks';
-
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { Table } from 'reactstrap';
-import Map from 'lodash/map';
+import { queries } from '../utils/constants';
 
-// const QUERIES = {
-//   car: `${baseurl}/car/api/noseque`
-// };
+const BaseTable = ({ entity }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [{ response, loading, err }, refetch] = useAxios(`${queries[entity]}`);
 
-const mockData = [
-  {
-    status: true,
-    _id: '5d77e3ba974d5e86c83fa930',
-    description: 'First car',
-    chasisNO: 'chasis1212',
-    motorNO: 'motor2121',
-    plateNO: 'oias21293',
-    carTypeID: '5d77e2b3974d5e86c83fa92f',
-    brandID: '5d771e2c605dac82e313b757',
-    modelID: '5d77e257974d5e86c83fa92e',
-    fuelTypeID: '5d77226523511e03976c80ba',
-    __v: 0
-  },
-  {
-    status: true,
-    _id: '5d7865d9e095578debff4705',
-    description: 'Second car',
-    chasisNO: 'chasis1212',
-    motorNO: 'motor2121',
-    plateNO: 'oias21293',
-    carTypeID: '5d77e2b3974d5e86c83fa92f',
-    brandID: '5d771e2c605dac82e313b757',
-    modelID: '5d77e257974d5e86c83fa92e',
-    fuelTypeID: '5d77226523511e03976c80ba',
-    carStatus: 'Rented',
-    __v: 0
+  function parseCar(data) {
+    return {
+      // eslint-disable-next-line no-underscore-dangle
+      id: data._id,
+      description: data.description,
+      chasis: data.chasisNO,
+      motor: data.motorNO,
+      plate: data.plateNO
+    };
   }
-];
-
-const BaseTable = ({ url }) => {
-  // const [dbData, setDBData] = useState([]);
-  // const [error, setError] = useState();
-  // const [keys, setKeys] = useState([]);
-
-  // const mappingKeys = () => {
-  //   mockData.map((data) => setKeys(Object.keys(data)));
-  // };
-
-  const [{ data, loading, err }, refetch] = useAxios('https://api.github.com/repos/zeit/next.js');
-  // function fetchData(query) {
-    // setError(error);
-    // setDBData(data);
-  // }
-
-  // useEffect(() => {
-  //   fetchData();
-  // }, []);
-
-  // useEffect(() => {
-  //   try {
-  //     setDBData(mockData);
-  //     mappingKeys();
-  //   }
-  //   catch (err) {
-  //     alert(err);
-  //   }
-  // }, [mockData]);
+  const transformedData = response && response.data ? response.data.map(parseCar) : null;
 
   return (
-    <>
-      <h1>tamo aqui</h1>
-      {err ? <div>error</div> : <pre>{JSON.stringify(data, null, 2)}</pre>}
-    </>
-    // <Table hover>
-    //   <thead>
-    //     <tr>
-    //       {keys.map((head) => (
-    //         <th key={head.id}>{head}</th>
-    //       ))}
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     {Map(dbData, (obj) => (
-    //       <tr>
-    //         {Object.values(obj).map((el) => (
-    //           <Link href={{
-    //             pathname: url,
-    //             query: { id: obj._id }
-    //           }}
-    //           ><td>{el}</td>
-    //           </Link>
-    //         ))}
-    //       </tr>
-    //     ))}
-    //   </tbody>
-    //   {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-    // </Table>
-  )
+    <Table hover>
+      <thead>
+        <tr>
+          {transformedData && Object.keys(transformedData[0]).filter((data) => data !== 'id').map((key) => (
+            <th key={key.id}>{key}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {transformedData && transformedData.map((element) => (
+          <tr>
+            {Object.values(element).map((field, i) => {
+              if (i) {
+                return (
+                  <Link href={`/${entity}/${element.id}`}>
+                    <td>{field}</td>
+                  </Link>
+                );
+              }
+              return null;
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
 };
 
 BaseTable.propTypes = {
-  // resData: PropTypes.arrayOf(
-  //   PropTypes.object
-  // ).isRequired,
-  url: PropTypes.string.isRequired
+  entity: PropTypes.string.isRequired
 };
 
 export default BaseTable;
--- pages/car.js
+++ pages/car.js
@@ -1,57 +1,19 @@
 import React from 'react';
 import Head from 'next/head';
-import axios from 'axios';
 import Layout from '../components/Layout';
 import withAuth from '../components/lib/withAuth';
 import BaseTable from '../components/BaseTable';
 
-const Cars = ({ userCookie, stars }) =>
-  // const dbData = [
-  //   {
-  //     status: true,
-  //     _id: '5d77e3ba974d5e86c83fa930',
-  //     description: 'First car',
-  //     chasisNO: 'chasis1212',
-  //     motorNO: 'motor2121',
-  //     plateNO: 'oias21293',
-  //     carTypeID: '5d77e2b3974d5e86c83fa92f',
-  //     brandID: '5d771e2c605dac82e313b757',
-  //     modelID: '5d77e257974d5e86c83fa92e',
-  //     fuelTypeID: '5d77226523511e03976c80ba',
-  //     __v: 0
-  //   },
-  //   {
-  //     status: true,
-  //     _id: '5d7865d9e095578debff4705',
-  //     description: 'Second car',
-  //     chasisNO: 'chasis1212',
-  //     motorNO: 'motor2121',
-  //     plateNO: 'oias21293',
-  //     carTypeID: '5d77e2b3974d5e86c83fa92f',
-  //     brandID: '5d771e2c605dac82e313b757',
-  //     modelID: '5d77e257974d5e86c83fa92e',
-  //     fuelTypeID: '5d77226523511e03976c80ba',
-  //     carStatus: 'Rented',
-  //     __v: 0
-  //   }
-  // ];
+const Cars = ({ userCookie }) =>
   (
     <div>
       <Head>
         <title>Cars</title>
       </Head>
       <Layout user={userCookie}>
-        <BaseTable url="car" />
+        <BaseTable entity="car" />
       </Layout>
     </div>
   );
-
-// Cars.getInitialProps = async () => {
-//   const res = await axios.get('https://api.github.com/repos/zeit/next.js');
-//   // const json = await JSON.parse(res);
-//   console.log(res, '***************');
-
-//   return { stars: res.data.stargazers_count };
-// };
 
 export default withAuth(Cars);
