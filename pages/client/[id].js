14593ccc0218db9a8a078e3f88cd665ed033672f
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-09-29 23:36:31 -0400

    adding single pages base template for all the services

---
+++ pages/client/[id].js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import useAxios from 'axios-hooks';
+import Layout from '../../components/Layout';
+import { queries } from '../../utils/constants';
+
+const SingleClient = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  // eslint-disable-next-line no-unused-vars
+  const [{ response, loading, err }, refetch] = useAxios(`${queries.client}/${id}`);
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Layout>
+        <h1>SingleClient</h1>
+        {response && response.data && <pre>{JSON.stringify(response.data, null, 2)}</pre>}
+
+      </Layout>
+    </div>
+  );
+};
+
+export default SingleClient;
