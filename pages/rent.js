cf6966d763229ff79a352d6bb0a4c6dc42262f6f
Ricardo <shirotokuro3715@gmail.com>
2019-10-08 12:37:23 -0400

    Added redirect from rent to insp

--- pages/rent.js
+++ pages/rent.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../components/Layout';
 import withAuth from '../components/lib/withAuth';
 import BaseTable from '../components/BaseTable';
@@ -10,6 +11,10 @@ const Rent = ({ userCookie }) =>
       <Head>
         <title>Car rents</title>
       </Head>
+      <p>To create a rent you <span><bold>Must</bold></span> create an inspection first</p>
+      <Link href="/inspection">
+        <button type="button">Inspections</button>
+      </Link>
       <BaseTable entity="rent" />
     </Layout>
   );
